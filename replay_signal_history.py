1a0039c08a1a5a7f5717ceb6a051231e54571e7b
tong1304 <kkg.2026.allnew@gmail.com>
2026-08-23 23:22:44 +0700

    chore: activate V9.2 historical replay

--- replay_signal_history.py
+++ replay_signal_history.py
@@ -1,6 +1,6 @@
-"""V9.1 historical replay entry point."""
-from replay_signal_history_v9_1 import *
-from replay_signal_history_v9_1 import main
+"""V9.2 historical replay entry point."""
+from replay_signal_history_v9_2 import *
+from replay_signal_history_v9_2 import main
 
 if __name__ == "__main__":
     main()
