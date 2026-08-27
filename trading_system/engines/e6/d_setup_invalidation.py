d194465d5e19314783b76878259ed395dd06dccd
tong1304 <kkg.2026.allnew@gmail.com>
2026-08-28 00:24:03 +0700

    Apply objective pullback gate to E6D

--- trading_system/engines/e6/d_setup_invalidation.py
+++ trading_system/engines/e6/d_setup_invalidation.py
@@ -1,6 +1,10 @@
 from ...core.subengine import SubEngine as _Base
+from .setup_logic import refine_e6
 
 
 class SubEngine(_Base):
     def __init__(self):
         super().__init__()
+
+    def _analyse(self, data):
+        return refine_e6(super()._analyse(data), data, self.sub_engine_id)
