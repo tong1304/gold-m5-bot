96b7b04599329b3a8d3537bd6573f2657ea0aedb
tong1304 <kkg.2026.allnew@gmail.com>
2026-08-28 10:58:37 +0700

    route E1 public interface to v11 core

--- production_v2/e1_professional_layer_v10.py
+++ production_v2/e1_professional_layer_v10.py
@@ -1,4 +1,6 @@
-"""E1 V10 public interface for the standalone professional core."""
-from .e1_professional_core_v10 import analyze_e1_professional_v10
+"""E1 V10-compatible public interface backed by the V11 professional core."""
+from .e1_professional_core_v11 import analyze_e1_professional_v11
 
-__all__ = ["analyze_e1_professional_v10"]
+analyze_e1_professional_v10 = analyze_e1_professional_v11
+
+__all__ = ["analyze_e1_professional_v10", "analyze_e1_professional_v11"]
