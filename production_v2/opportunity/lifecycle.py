c566bfa8af6c0cf350a753f761110c1ba42d5e93
tong1304 <kkg.2026.allnew@gmail.com>
2026-09-05 20:39:11 +0700

    refactor: expose canonical opportunity lifecycle namespace

---
+++ production_v2/opportunity/lifecycle.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from ..opportunity_lifecycle import (
+    ACTIVE_STATES,
+    MAX_WATCH_BARS,
+    TERMINAL_STATES,
+    VALID_DIRECTIONS,
+    WATCH_SETUPS,
+    advance_opportunity,
+)
+
+__all__ = [
+    "ACTIVE_STATES",
+    "MAX_WATCH_BARS",
+    "TERMINAL_STATES",
+    "VALID_DIRECTIONS",
+    "WATCH_SETUPS",
+    "advance_opportunity",
+]
